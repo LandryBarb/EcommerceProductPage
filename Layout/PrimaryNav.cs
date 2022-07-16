4b29b6c7afe535373e91006740784083625709db
Landry <landry@landrybarb.com>
2022-07-16 07:50:28 -0700

    Header Styling & Resposive

---
+++ Layout/PrimaryNav.cs
@@ -0,0 +1,10 @@
+namespace EcommerceProductPage.Layout
+{
+    public class PrimaryNav
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Url { get; set; }
+
+    }
+}
